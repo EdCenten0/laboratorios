5d8ebf4208f6cc6c9f3f5123913878663abacb57
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-02-08 20:24:55 -0600

    Tabla de clases por profesor 10%

--- src/main/java/laboratorios_DAO/Dt_clases_por_profesor.java
+++ src/main/java/laboratorios_DAO/Dt_clases_por_profesor.java
@@ -4,10 +4,13 @@
  */
 package laboratorios_DAO;
 
+import com.mysql.cj.protocol.Resultset;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import laboratorios_entities.Clases_por_profesor;
 import laboratorios_interface_DAO.Clases_por_profesor_interface;
 
@@ -17,11 +20,11 @@ import laboratorios_interface_DAO.Clases_por_profesor_interface;
  */
 public class Dt_clases_por_profesor implements Clases_por_profesor_interface{
     
-    final String INSERT = "INSERT INTO clases_por_profesor (id_profesor, id_clase) VALUES (?, ?)";
-    final String UPDATE = "UPDATE clases_por_profesor SET id_profesor = ?, id_clase = ? WHERE id = ?";
-    final String DELETE = "DELETE FROM clases_por_profesor WHERE id = ?";
-    final String GETALL = "SELECT id, codigo_clase, nombre_clase, cantidad_estudiantes_clase, cantidad_creditos FROM clases";
-    final String GETONE = "SELECT id, codigo_clase, nombre_clase, cantidad_estudiantes_clase, cantidad_creditos WHERE id = ?";    
+    final String INSERT = "INSERT INTO laboratorios.clases_por_profesor (id_profesor, id_clase) VALUES (?, ?)";
+    final String UPDATE = "UPDATE laboratorios.clases_por_profesor SET id_profesor = ?, id_clase = ? WHERE id = ?";
+    final String DELETE = "DELETE FROM laboratorios.clases_por_profesor WHERE id = ?";
+    final String GETALL = "SELECT * FROM laboratorios.clases_por_profesor";
+    final String GETONE = "SELECT * FROM laboratorios.clases_por_profesor WHERE id = ?";     
     private Connection conn;
 
     @Override
@@ -96,14 +99,90 @@ public class Dt_clases_por_profesor implements Clases_por_profesor_interface{
         }
     }
 
-    @Override
-    public List<Clases_por_profesor> obtenerTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    private Clases_por_profesor convertir_clases_Clases_por_profesor(ResultSet rs) throws SQLException{
+        Long id = rs.getLong("id");
+        Long id_profesor = rs.getLong("id_profesor");
+        Long id_clase = rs.getLong("id_clase");
+        
+        Clases_por_profesor clases_por_profesor = new Clases_por_profesor(id_profesor, id_clase);
+        clases_por_profesor.setId(id);
+        return clases_por_profesor;
+        
+        
     }
 
     @Override
-    public Clases_por_profesor obtener(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<Clases_por_profesor> obtenerTodos() throws DAO_exception {
+         PreparedStatement stat = null;
+        ResultSet rs = null;
+        List<Clases_por_profesor> c = new ArrayList<>();
+        try {
+            stat = conn.prepareStatement(GETALL);
+            rs = stat.executeQuery();
+            while(rs.next()){
+                c.add(convertir_clases_Clases_por_profesor(rs));
+            }
+        } catch (SQLException e) {
+            throw new DAO_exception("Error en SQL", e);
+        }finally{
+            if(rs != null){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAO_exception("Error al cerrar la conexion de ResultSet", e);
+                    
+                }
+            }
+            
+            if (stat != null) {
+                try {
+                    stat.close();
+                } catch (SQLException e) {
+                    throw new DAO_exception("Error al cerrar la conexion de PrepareStatement", e);
+                }
+            }
+        }
+        
+        return c;
+    }
+
+    @Override
+    public Clases_por_profesor obtener(Long id) throws DAO_exception {
+        PreparedStatement stat = null;
+        ResultSet rs = null;
+        Clases_por_profesor c = null;
+        try {
+            stat = conn.prepareStatement(GETONE);
+            stat.setLong(1, id);
+            rs = stat.executeQuery();
+            if(rs.next()){
+                c = convertir_clases_Clases_por_profesor(rs);
+                
+            }else{
+                throw new DAO_exception("No se ha encontrado ese registro");
+            }
+        } catch (SQLException e) {
+            throw new DAO_exception("Error en SQL", e);
+        } finally{
+            if(rs != null){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAO_exception("Error al cerrar la conexion de ResultSet", e);
+                    
+                }
+            }
+            
+            if (stat != null) {
+                try {
+                    stat.close();
+                } catch (SQLException e) {
+                    throw new DAO_exception("Error al cerrar la conexion de PrepareStatement", e);
+                }
+            }
+        }
+        
+        return c;
     }
     
 }
