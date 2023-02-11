a0cc38aa6d8ef6bdb1ca2d91e3d37bbd7cbbdc2e
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-02-10 21:40:59 -0600

    TableModel de clases por profesor terminado

--- src/main/java/laboratorios_presentacion/ClasesPorProfesorTableModel.java
+++ src/main/java/laboratorios_presentacion/ClasesPorProfesorTableModel.java
@@ -28,6 +28,8 @@ class ClasesPorProfesorTableModel extends AbstractTableModel {
     private List<Profesores> profesores = new ArrayList<>();
     private List<Clases> clases = new ArrayList<>();
     
+       
+    
     
     public void updateModel() throws DAO_exception{
         this.datos = clasesPorProfesorDAO.obtenerTodos();
@@ -67,36 +69,38 @@ class ClasesPorProfesorTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-       Clases_por_profesor preguntado = this.datos.get(rowIndex);
-       Profesores preguntadoProfesores = new Profesores();
-       Clases preguntadoClases = new Clases();
-       int i = 0;
-       
-       do{
-           if(this.clases.get(i).getId() == preguntado.getId_clase()){
-              preguntadoClases.setNombre_clase(this.clases.get(i).getNombre_clase());
-              preguntadoClases.setCodigo_clase(this.clases.get(i).getCodigo_clase());
-              break;
-           }else{
-               i++;
-           }
-       }while(this.clases.get(i).getId() != preguntado.getId_clase());
-       
-       do{
-           if(this.profesores.get(i).getId() == preguntado.getId_profesor()){
-              preguntadoProfesores.setNombre_profesor(this.profesores.get(i).getNombre_profesor());
-              preguntadoProfesores.setApellido_profesor(this.profesores.get(i).getApellido_profesor());
-              preguntadoProfesores.setCodigo_profesor(this.profesores.get(i).getCodigo_profesor());
-              break;
-           }else{
-               i++;
-           }
-       }while(this.profesores.get(i).getId() != preguntado.getId_profesor());
-       
+       Clases_por_profesor preguntado = this.datos.get(rowIndex);     
        switch(columnIndex){
            case 0: return preguntado.getId();
-           case 1: return preguntadoClases.getNombre_clase().concat(" -- ".concat(preguntadoClases.getCodigo_clase()));
-           case 2: return preguntadoProfesores.getNombre_profesor().concat(" ").concat(preguntadoProfesores.getApellido_profesor().concat(" -- ").concat(preguntadoProfesores.getCodigo_profesor()));
+           case 1: 
+               int i = 0;
+               Clases preguntadoClases = new Clases();
+
+               while(this.clases.get(i).getId() != preguntado.getId_clase() || this.clases.get(i).getId() == preguntado.getId_clase()){
+                    if(this.clases.get(i).getId() == preguntado.getId_clase()){
+                      preguntadoClases.setNombre_clase(this.clases.get(i).getNombre_clase());
+                      preguntadoClases.setCodigo_clase(this.clases.get(i).getCodigo_clase());
+                      break;
+                    }else{
+                       i++;
+                        }   
+                    }
+               return preguntadoClases.getNombre_clase().concat(" -- ".concat(preguntadoClases.getCodigo_clase()));
+           case 2: 
+               int j = 0;
+               Profesores preguntadoProfesores = new Profesores();
+               while(this.profesores.get(j).getId() != preguntado.getId_profesor() || this.profesores.get(j).getId() == preguntado.getId_profesor()){
+                    if(this.profesores.get(j).getId() == preguntado.getId_profesor()){
+                    preguntadoProfesores.setNombre_profesor(this.profesores.get(j).getNombre_profesor());
+                    preguntadoProfesores.setApellido_profesor(this.profesores.get(j).getApellido_profesor());
+                    preguntadoProfesores.setCodigo_profesor(this.profesores.get(j).getCodigo_profesor());
+                    break;
+                }else{
+                    j++;
+                }
+                }
+               
+               return preguntadoProfesores.getNombre_profesor().concat(" ").concat(preguntadoProfesores.getApellido_profesor().concat(" -- ").concat(preguntadoProfesores.getCodigo_profesor()));
            default: return "Error en la tabla";
        }
     }
